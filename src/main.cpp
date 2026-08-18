f406136cbe1b87ee3dcc3acbe50d3017d83ad25e
akiakyo <you@example.com>
2026-08-19 04:46:32 +0800

    added: initial cmake project setup

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "==========================" << endl;
+    cout << "      AkyMonitor v0.1      " << endl;
+    cout << "==========================" << endl;
+
+    cout << endl;
+
+    cout << "System Monitor Starting..." << endl;
+
+    return 0;
+}
